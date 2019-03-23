1040acb42e3f17948052d64aef4ba50baab5efc6
DU-ds <kjcolins1759@gmail.com>
2019-03-23 14:55:02 -0400

    "Version 4"

--- identityMatrix.f
+++ identityMatrix.f
@@ -1,6 +1,21 @@
-      function identityM(n)
+
+
+      program identityMatrix
+      implicit none
+      integer :: n1
+      integer, allocatable :: array1(:,:)
+      print *, "How large an identity matrix do you want?"
+      read *, n1
+      allocate(array1( n1, n1))
+      identM(n1, array1)
+      call printMatrix( array1, n1)
+
+      end program identityMatrix
+
+      function identM(n, matrix1)
       integer, intent(in) :: n !input to the function
-      integer, dimension(n,n) :: x1 !output - n x n identity matrix
+      integer, dimension(n,n)   :: x1 !n x n identity matrix
+      integer, dimension(n,n), intent(out) :: matrix1 
       integer :: col, row
       do row = 1, n
         do col = 1, n
@@ -11,21 +26,15 @@
           end if
         end do
       end do
-      end function identityM
+      matrix1 = x1
+      end function identM
 
-      program identityMatrix
-      implicit none
-      integer :: col, row
-      integer, parameter :: n1 = 10, n2 = n1 !I'd like to go back to n instead of n1, n2
-      integer,  dimension(n1,n1) :: x1 
-      do row = 1, n1
-        do col = 1, n1
-          if (col == row) then
-            x1(row, col) = 1
-          else
-            x1(row, col) = 0
-          end if
+      subroutine printMatrix(array, n2) ! if this was to print a n x k matrix, I'd put a k on line 3, a k arg
+        implicit none !in the subroutine and repalce the second n in the array(n,n) with a k
+        integer, intent(in) :: array(n2,n2)  !creating an n x n identity matrix
+        integer, intent(in) :: n2 !declaring that it's an input? IDK lol
+        integer :: i
+        do i = 1, n2
+          print*, array(i,:)
         end do
-      end do
-      end program identityMatrix
-      !so this part of the identity matrix program isnt working :/
+      end subroutine printMatrix

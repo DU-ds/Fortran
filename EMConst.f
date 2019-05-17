8e45d283832cf1d8c88592e4ed6472ca54307e29
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-05-17 00:30:17 -0400

    "now calculates for the max 32-bit(4 byte) int"

--- EMConst.f
+++ EMConst.f
@@ -7,13 +7,17 @@
 
       
       program EMConst
-         implicit none
-         integer :: n
+C          implicit none
+C https://gcc.gnu.org/onlinedocs/gfortran/Fortran-Dialect-Options.html
+C https://stackoverflow.com/questions/10884260/how-can-gfortran-tell-if-i-am-compiling-f90-or-f95-code
+         use ISO_FORTRAN_ENV 
+C          https://stackoverflow.com/questions/3170239/fortran-integer4-vs-integer4-vs-integerkind-4#3170438
+         integer(kind=int32) :: n
          integer, parameter :: rp = selected_real_kind(15)
-         integer :: i
+         integer (kind = int32) :: i
          Real (kind = rp) :: b
          Real (kind = rp) :: a
-         n = 1000000000
+         n = huge(n) ! https://stackoverflow.com/questions/9569756/fortran-the-largest-and-the-smallest-integer
          a = 0.0_rp
          do i = 1,n,1
             a = (a + (1.0_rp/i))         
@@ -21,10 +25,6 @@
          b = log(real(n)) !https://gcc.gnu.org/onlinedocs/gfortran/REAL.html
          print *, a - b
       end program EMConst
-
-
-
-
 
 
 C gfortran EMConst.f -o EMConst.out
